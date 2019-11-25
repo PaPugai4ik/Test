6809aca7f0c9a3f350bfbb71eda7fcb22a0f7b29
Papugai4ik <balysh1993@mail.ru>
2019-11-25 14:26:07 +0300

    1

---
+++ Svarca_Test/Assets/Script/SettingsSvarca.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Soed")]
+public class SettingsSvarca : ScriptableObject
+{
+    public GameObject Soed;
+    [SerializeField]
+    [Range(1, 4)]
+    int D;
+    private int I=0;
+    private void OnEnable()
+    {
+        switch (D)
+        {
+            case 1:I = 30;break;
+            case 2: I = 65; break;
+            case 3: I = 105; break;
+            case 4: I = 150; break;
+        }
+    }
+}
