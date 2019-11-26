df7013c6b0e47c72ff60063261fc364e6394d96b
Papugai4ik <balysh1993@mail.ru>
2019-11-26 21:08:23 +0300

    Сварка Signed-off-by: Papugai4ik <balysh1993@mail.ru>

--- Svarca_Test/Assets/Script/SettingsSvarca.cs
+++ Svarca_Test/Assets/Script/SettingsSvarca.cs
@@ -8,7 +8,14 @@ public class SettingsSvarca : MonoBehaviour
 {
     string[] start = new string[] { "3", "2", "1", "Go!" };
     [SerializeField] GameObject t_Timer;
+    [SerializeField] GameObject Show;
+    [SerializeField] GameObject Soed;
+
     [SerializeField]GameObject point;
+
+    Vector3 Pos;
+    Vector3 dir=new Vector3(0.3f,0.3f,0);
+    GameObject obj;
     float Sensitivity=1;
     int Timer = 4;
     [SerializeField] GameObject Panel;
@@ -29,6 +36,45 @@ public class SettingsSvarca : MonoBehaviour
 
         if (Cursor.visible == false)
         {
+            if (Input.GetMouseButton(0))
+            {
+                RaycastHit hit;
+                Ray ray = new Ray(GameObject.FindGameObjectWithTag("Spawn").transform.position,new Vector3(0,5,5));
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (obj == null)
+                    {
+                        obj = Instantiate(Show, hit.point, Quaternion.identity);
+                        Pos = point.transform.position;
+                    }
+                    if (point.transform.position.y > (Pos.y + dir.y) ||
+                        point.transform.position.y < (Pos.y - dir.y) ||
+                        point.transform.position.x > (Pos.x + dir.x) ||
+                        point.transform.position.x < (Pos.x - dir.x))
+                    {
+                        onStartShow();
+                        obj = Instantiate(Show, hit.point, Quaternion.identity);
+                        obj.transform.parent = Soed.transform;
+                        Pos = point.transform.position;
+                    }
+                    else
+                    {
+                        if (obj.transform.localScale == new Vector3(4, 4, 1))
+                        {
+                            Destroy(obj);
+                        }
+                        else
+                        {
+                            obj.transform.localScale += new Vector3(0.1f, 0.1f, 0);
+                        }
+                    }
+                }
+                else
+                {
+                    if(obj!=null)
+                    onStartShow();
+                }
+            }
             point.transform.Translate(new Vector3(Input.GetAxis("Mouse X") * Sensitivity, Input.GetAxis("Mouse Y") * Sensitivity, 0));
         }
 
@@ -36,6 +82,10 @@ public class SettingsSvarca : MonoBehaviour
         {
             Panel.SetActive(!Panel.activeSelf);
         }
+    }
+    public void onStartShow()
+    {
+        obj.GetComponentInChildren<Timer>().Active();
     }
     IEnumerator StartSvarca()
     {
---
+++ Svarca_Test/Assets/Script/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+     bool isActive = false;
+    float i = 0.7f;
+    private void Update()
+    {
+        if (isActive)
+        {
+            if (i<0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                i -= Time.deltaTime;
+            }
+            
+        }
+    }
+    public void Active() {
+        isActive = true;
+    }
+}
